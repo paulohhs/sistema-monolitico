f276e3af60b825bddc6c226f89a426d825d0273e
paulohhs <paulohhonoratos@gmail.com>
2026-09-16 16:56:06 -0300

    product module - usecase(src/modules/product-adm/usecase/add-product/add-product.dto.ts): adiciona dtos para AddProduct usecase.

---
+++ src/modules/product-adm/usecase/add-product/add-product.dto.ts
@@ -0,0 +1,17 @@
+export default interface AddProductInputDto {
+    id?: string;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+}
+
+export interface AddProductOutputDto {
+    id: string;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
