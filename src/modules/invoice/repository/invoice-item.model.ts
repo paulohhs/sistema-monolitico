6cd99e1e52b126d4deec7c477820fe1c48c6831b
paulohhs <paulohhonoratos@gmail.com>
2026-09-21 15:54:25 -0300

    invoice module - repository(src/modules/invoice/repository/invoice-item.model.ts): implementa InvoiceItem model

---
+++ src/modules/invoice/repository/invoice-item.model.ts
@@ -0,0 +1,22 @@
+import { Column, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import InvoiceModel from "./invoice.model";
+
+@Table({
+    tableName: "invoice_items",
+    timestamps: false,
+})
+export default class InvoiceItemModel extends Model {
+    @PrimaryKey
+    @Column({ allowNull: false })
+    id: string;
+
+    @Column({ allowNull: false })
+    name: string;
+    
+    @Column({ allowNull: false })
+    price: number;
+
+    @ForeignKey(() => InvoiceModel)
+    @Column({allowNull: false})
+    declare invoice_id: string;
+}
