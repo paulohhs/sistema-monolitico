7c548aa2a0a1ff9e315183b54d66b830bee3722b
paulohhs <paulohhonoratos@gmail.com>
2026-09-21 15:50:27 -0300

    invoice module - usecase(src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.spec.ts): adiciona testes para GenerateInvoice usecase

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.spec.ts
@@ -0,0 +1,52 @@
+import GenerateInvoiceUseCase from "./generate-invoice-.usecase";
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+};
+
+describe("GenerateInvoice usecase unit test", () => {
+  it("should generate an invoice", async () => {
+    const invoiceRepository = MockRepository();
+    const usecase = new GenerateInvoiceUseCase(invoiceRepository);
+
+    const input = {
+      name: "Invoice Test",
+      document: "Document Test",
+      street: "Street Test",
+      number: "1",
+      complement: "",
+      city: "City Test",
+      state: "State Test",
+      zipCode: "ZipCode Test",
+      items: [{
+          id: "1",
+          name: "Invoice Item Test",
+          price: 100,
+      }],
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(invoiceRepository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined;
+    expect(result.name).toEqual(input.name);
+    expect(result.document).toEqual(input.document);
+    expect(result.street).toEqual("Street Test");
+    expect(result.number).toEqual("1");
+    expect(result.complement).toEqual( "");
+    expect(result.city).toEqual("City Test");
+    expect(result.state).toEqual("State Test");
+    expect(result.zipCode).toEqual("ZipCode Test");
+    expect(result.items).toEqual([
+        {
+            id: input.items[0].id,
+            name: input.items[0].name,
+            price: input.items[0].price,
+        }
+    ]);
+    expect(result.total).toEqual(100);
+  });
+});
