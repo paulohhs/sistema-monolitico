0f82bd1497b7ab906ed5961c82f918780cad2eaa
paulohhs <paulohhonoratos@gmail.com>
2026-09-23 20:11:08 -0300

    refactor(src/modules/product-adm/facade/product-adm.facade.interface.ts): ajusta interface da facade para ter dtos de retorno.

--- src/modules/product-adm/facade/product-adm.facade.interface.ts
+++ src/modules/product-adm/facade/product-adm.facade.interface.ts
@@ -6,6 +6,17 @@ export interface AddProductFacadeInputDto {
     stock: number;
 }
 
+export interface AddProductFacadeOutputDto {
+    id: string;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    salesPrice: number;
+    stock: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 export interface CheckStockFacadeInputDto {
     productId: string;
 }
@@ -16,6 +27,6 @@ export interface CheckStockFacadeOutputDto {
 }
 
 export default interface ProductAdmFacadeInterface {
-    addProduct(input: AddProductFacadeInputDto): Promise<void>;
+    addProduct(input: AddProductFacadeInputDto): Promise<AddProductFacadeOutputDto>;
     checkStock(input: CheckStockFacadeInputDto): Promise<CheckStockFacadeOutputDto>;
 }
